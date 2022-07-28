3724eceff1a8e572cd6582db23e89b2f4b82b788
Abhishek Naidu <abhishek.naidu@kohler.com>
2022-07-28 13:36:32 +0530

    Changed Name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 const server = http.createServer((req,res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
-    res.end('Hello Abhishek\n');
+    res.end('Hello People\n');
 });
 
 server.listen(PORT, HOSTNAME, () => {
